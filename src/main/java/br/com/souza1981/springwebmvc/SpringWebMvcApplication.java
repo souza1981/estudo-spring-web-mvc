b9b8731dc01003a7b327c87d740f88c47c70a5c4
Souza1981 <souza1981@gmail.com>
2020-06-03 21:33:21 -0300

    Versão inicial

---
+++ src/main/java/br/com/souza1981/springwebmvc/SpringWebMvcApplication.java
@@ -0,0 +1,13 @@
+package br.com.souza1981.springwebmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebMvcApplication.class, args);
+	}
+
+}
